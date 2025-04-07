f47098fe3ed6e120c452336ca76f3f78f6e22957
David Wu <david.wu@microlife.com.tw>
2025-04-07 11:35:12 +0800

    20250407 -add Bottom Tabs Navigation

---
+++ src/pages/profilescreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+function ProfileScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+}
+export default ProfileScreen;
